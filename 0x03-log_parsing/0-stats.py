1ec7c1252a7d33381ab68e014f77c030cc9e7c68
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-25 03:14:55 +0300

    Commit on 2024-10-25 03:14:55

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,57 @@
 #!/usr/bin/python3
-"""Python script that reads stdin line by line and computes metrics"""
-
 import sys
+import re
+import signal
+
+# Initialize counters
+total_file_size = 0
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+                      404: 0, 405: 0, 500: 0}
+line_count = 0
+
+lf1 = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}-\[.*?\] \"GET /projects/260 "
+lf2 = r"HTTP/1\.1\" (\d{3}) (\d+)$"
+log_format_regex = re.compile(lf1 + lf2)
 
 
-def print_n(t_file_size, status):
-    """Prints total file size and status list"""
-    print("File size: {:d}".format(t_file_size))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+def print_statistics():
+    """Prints the accumulated statistics."""
+    print(f"File size: {total_file_size}")
+    for code in sorted(status_code_counts.keys()):
+        if status_code_counts[code] > 0:
+            print(f"{code}: {status_code_counts[code]}")
 
 
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
+def signal_handler(sig, frame):
+    """Handles the signal for graceful shutdown."""
+    print_statistics()
+    sys.exit(0)
 
-t_file_size = 0
-count = 0
+
+# Set up the signal handler for CTRL + C
+signal.signal(signal.SIGINT, signal_handler)
+
+# Read from stdin line by line
 try:
     for line in sys.stdin:
-        args = line.split()
-
-        if len(args) > 2:
-            status_code = args[-2]
-            file_size = int(args[-1])
-
-            if status_code in status:
-                status[status_code] += 1
-
-            t_file_size += file_size
-            count += 1
-
-            if count == 10:
-                print_n(t_file_size, status)
-                count = 0
+        line = line.strip()
+        match = log_format_regex.match(line)
+        if match:
+            status_code = int(match.group(1))
+            file_size = int(match.group(2))
+            # Update the total file size and the corresponding
+            total_file_size += file_size
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+        line_count += 1
+        # Print the statistics every 10 lines
+        if line_count % 10 == 0:
+            print_statistics()
 
 except KeyboardInterrupt:
-    pass
+    # Print the statistics when interrupted by CTRL + C
+    print_statistics()
+    sys.exit(0)
 
-finally:
-    print_n(t_file_size, status)
+# Print the final statistics if the loop finishes naturally
+print_statistics()
